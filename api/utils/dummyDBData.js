eaea39801cf9e4f195fa547de0e0a0c685978e49
Waheed Afolabi <waheed.freelancer16@gmail.com>
2019-02-17 09:29:33 +0100

    Feature - Created meal service and dummy DB data

---
+++ api/utils/dummyDBData.js
@@ -0,0 +1,28 @@
+export default {
+  meals: [
+    {
+      id: 1,
+      name: 'Amala with gbegiri',
+      price: '450',
+      description: 'A little description',
+    },
+    {
+      id: 2,
+      name: 'Iyan with efo-riro',
+      price: '550',
+      description: 'A little description',
+    },
+    {
+      id: 3,
+      name: 'Jollof rice with salad',
+      price: '300',
+      description: 'A little description',
+    },
+    {
+      id: 4,
+      name: 'Salad with plantain',
+      price: '350',
+      description: 'A little description',
+    },
+  ],
+};
