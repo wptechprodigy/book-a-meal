e743c2ea0b90c3f58562f0106707592701103a6b
Waheed Afolabi <waheed.freelancer16@gmail.com>
2019-03-05 13:25:48 +0100

    Feature/Created user model

---
+++ api/models/user.js
@@ -0,0 +1,28 @@
+import Sequelize from 'sequelize';
+import sequelize from '../utils/database';
+
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  createdAt: Sequelize.DATEONLY,
+  updatedAt: Sequelize.DATEONLY
+});
+
+export default User;
